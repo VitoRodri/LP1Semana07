e7a10963d0422db54a71717a818f46cf9061b438
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-04-27 23:20:11 +0100

    Create FillSpheres

---
+++ FillSpheres/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FillSpheres
+{  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Color
+    {
+        private byte r;
+        private byte g;
+        private byte b;
+        private byte a;
+
+        
+    }
+
+    class Sphere
+    {
+
+    }
+}
